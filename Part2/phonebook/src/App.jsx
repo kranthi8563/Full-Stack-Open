5ffe4cb15a575c6be44d1fe38ff04fe8b343a442
kranthi8563 <kranthireddy603@gmail.com>
2025-05-14 15:21:58 +0530

    Exercise 2.6

---
+++ Part2/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+const addName=(event)=>{
+  event.preventDefault()
+  setPersons(persons.concat({name : newName}))
+  setNewName("")
+}
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={(event)=>setNewName(event.target.value)}/>
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+   <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
