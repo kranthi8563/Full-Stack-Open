8ea275f607621fb5a8534791cb2941f85158358d
kranthi8563 <kranthireddy603@gmail.com>
2025-05-16 21:14:19 +0530

    Exercise 2.9

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   
   const [newNumber,setNewNumber]=useState('')
+   const [searchTerm, setSearchTerm] = useState('')
 const addName=(event)=>{
   event.preventDefault()
   if (persons.some(person => person.name === newName) || persons.some(person=>person.num===newNumber)) {
@@ -17,12 +18,24 @@ const addName=(event)=>{
   setNewName("")
   setNewNumber("")
 }
-
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    person.num.includes(searchTerm)
+  )
 
 
   return (
     <div>
       <h2>Phonebook</h2>
+       <div>
+        <h3>Search</h3>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search by name or number"
+        />
+      </div>
       <form onSubmit={addName} >
         <div>
           name: <input value={newName} onChange={(event)=>setNewName(event.target.value)}/>
@@ -33,10 +46,11 @@ const addName=(event)=>{
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-   
-        {persons.map((person) => <div key={person.name}>{person.name} {person.num}</div>)}
-     </div>
+     <div>
+        {filteredPersons.map(person => (
+          <div key={person.name}>{person.name} {person.num}</div>
+        ))}
+      </div>
     </div>
   )
 }
